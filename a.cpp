9eacdd23ce23f9303669e2ba1ed4d385d9015e00
Joshua Yun <joshuayun1999@gmail.com>
2019-02-05 17:25:10 -0800

    Add files via upload

---
+++ a.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+void rvereseArray(int arr[], int end)
+{
+    int start = 0;
+    while (start < end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+   for (int i = 0; i < size; i++)
+   cout << arr[i] << " ";
+   cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4};
+    printArray(arr, 4);
+    rvereseArray(arr, 3);
+    cout << "Reversed array is" << endl;
+    printArray(arr, 4);
+    return 0;
+}
